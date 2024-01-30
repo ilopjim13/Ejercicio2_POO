6f0bcdd41ca12dbbb4064582345884764663dc73
ivanjim13 <ivangorrion57@gmail.com>
2024-01-30 05:14:20 +0100

    terminado

--- src/Main.kt
+++ src/Main.kt
@@ -1,39 +1,24 @@
 fun main() {
-    val persona1 = Personas(67.87, 1.78)
-    val persona2 = Personas("Pepe",86.13, 1.98)
-    val persona3 = Personas("Jose",5.0, 1.00)
+    val persona1 = Persona(67.87, 1.78)
+    val persona2 = Persona("Pepe",86.13, 1.98)
+    val persona3 = Persona("Jose",5.0, 1.00)
 
 
     print("Persona1 introduzca su nombre: ")
     persona1.nombre = readln()
 
 
-    val persona4 = Personas("Ruperto",57.87, 1.40)
-    val persona5 = Personas("Antonio",66.13, 1.88)
-    val persona6 = Personas("Joselito",5.0, 1.00)
-    val persona7 = Personas("Manolo",77.88, 1.68)
+    val persona4 = Persona("Ruperto",57.87, 1.40)
+    val persona5 = Persona("Antonio",66.13, 1.88)
+    val persona6 = Persona("Joselito",5.0, 1.00)
+    val persona7 = Persona("Manolo",77.88, 1.68)
 
+    val listaPersonas = listOf(persona1, persona2, persona3, persona4, persona5, persona6,persona7)
 
-    persona1.saludar()
-    persona1.mostrarDesc()
-
-    persona2.saludar()
-    persona2.mostrarDesc()
-
-    persona3.saludar()
-    persona3.mostrarDesc()
-
-    persona4.saludar()
-    persona4.mostrarDesc()
-
-    persona5.saludar()
-    persona5.mostrarDesc()
-
-    persona6.saludar()
-    persona6.mostrarDesc()
-
-    persona7.saludar()
-    persona7.mostrarDesc()
+    listaPersonas.forEach {
+        it.saludar()
+        it.mostrarDesc()
+    }
 
 
 }
--- src/Persona.kt
+++ src/Persona.kt
@@ -1,28 +1,54 @@
-class Personas(val peso: Double, val altura: Double) {
+/**
+ * Clase persona
+ * @param peso :Double peso de la persona
+ * @param altura :Double altura de la persona
+ */
+class Persona(peso: Double, altura: Double) {
 
-    var nombre:String = ""
+    var peso = peso
+        set(value) {
+            require(value > 0) { "El peso no puede ser negativo" }
+            field = value
+        }
+    var altura: Double = altura
 
-    var imc: Double
-    init {
-        this.imc = peso / (altura * altura)
-    }
+    var nombre:String? = "persona"
+        set(value) {
+            require(!value.isNullOrBlank()) { "EL nombre no puede ser nulo ni vacío." }
+            field = value
+        }
+
+    val imc: Double
+        get() = peso / altura
 
     constructor(nombre:String, peso:Double, altura:Double) : this(peso, altura) {
         this.nombre = nombre
     }
 
-
+    /**
+     * Realiza la accion de saludar, y dice su nombre
+     */
     fun saludar() {
         println("Hola buenas noches, mi nombre es ${this.nombre}")
     }
 
-    private fun obtenerImc():Double {
-        return this.peso / (this.altura * this.altura)
-    }
+    /**
+     * Devuelve true o false si supera la media de altura
+     */
+    fun alturaEncimaMedia() = if (this.altura >= 1.75) true else false
 
+    /**
+     * Devuelve true o false si supera la media de peso
+     */
+    fun pesoEncimaMedia() = if (this.peso >= 70) true else false
+
+
+    /**
+     * Compara si las dos personas son iguales o no
+     */
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
-        if (other !is Personas) return false
+        if (other !is Persona) return false
 
         if (this.nombre != other.nombre) return false
         if (this.altura != other.altura) return false
@@ -30,14 +56,34 @@ class Personas(val peso: Double, val altura: Double) {
         return true
     }
 
+    /**
+     * Muestra la informacion de la persona con su altura, su pero y su imc.
+     */
     fun mostrarDesc() {
-        println("${this.nombre} con una altura de ${this.altura}m y un peso de ${this.peso}Kg tiene un IMC de ${String.format("%.2f",this.imc)} (${obtenerImcDesc()})")
+        println("${this.nombre} con una altura de ${this.altura}m (${comprobarMediaA()}) y un peso de ${this.peso}Kg (${comprobarMediaP()}) tiene un IMC de ${String.format("%.2f",this.imc)} (${obtenerImcDesc()})")
     }
 
+    /**
+     * Comprueba el peso de la persona y retorna un String con la frase correspondiente
+     * @return String frase indicativa correspondiente a un imc o a otro.
+     */
     private fun obtenerImcDesc() = when {
         this.imc < 18.5 -> "peso insuficiente"
         this.imc in 18.5..24.9 -> "peso saludable"
         this.imc in 25.0..29.9 -> "sobrepeso"
         else -> "obesidad"
     }
+
+    /**
+     * Comprueba si supera la altura media y devuelve si esta por encima de la media o no
+     * @return String devuleve si esta por encima de la media o no.
+     */
+    fun comprobarMediaA() = if (alturaEncimaMedia()) "Por encima de la media" else "Por debajo de la media"
+
+    /**
+     * Comprueba si supera el peso medio y devuelve si esta por encima de la media o no
+     * @return String devuleve si esta por encima de la media o no.
+     */
+    fun comprobarMediaP() = if (pesoEncimaMedia()) "Por encima de la media" else "Por debajo de la media"
+
 }
